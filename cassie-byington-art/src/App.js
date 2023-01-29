21890284ac592c2f3cd398739aa2c8931b905f9d
bg <62928419+bgeertsen@users.noreply.github.com>
2023-01-28 19:28:00 -0800

    created basic header

--- cassie-byington-art/src/App.js
+++ cassie-byington-art/src/App.js
@@ -1,5 +1,8 @@
-import "./App.css";
+import React from "react";
+import Header from "./components/Header";
 
-function App() {}
+function App() {
+  return <Header />;
+}
 
 export default App;
--- cassie-byington-art/src/components/Header/index.js
+++ cassie-byington-art/src/components/Header/index.js
@@ -1,5 +1,19 @@
 import React from "react";
-
+import logo from "../../assets/logo.webp";
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt={"Cassie Byington Art logo"} />
+      </div>
+      <nav>
+        <h2>Home</h2>
+        <h2>Gallery</h2>
+        <h2>About</h2>
+        <h2>Shop</h2>
+      </nav>
+    </header>
+  );
 };
+
+export default Header;
