4c1c36fa115bd6857afdd4377d0617da278a48b9
bg <62928419+bgeertsen@users.noreply.github.com>
2023-01-30 17:58:57 -0800

    added images to carousel

--- cassie-byington-art/src/components/Carousel/index.js
+++ cassie-byington-art/src/components/Carousel/index.js
@@ -1,46 +1,27 @@
 import Carousel from "react-bootstrap/Carousel";
+import placeholder1 from "../../assets/placeholder1.jpg";
+import placeholder2 from "../../assets/placeholder2.jpg";
+import placeholder3 from "../../assets/placeholder3.jpg";
 
 function UncontrolledExample() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="container">
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={placeholder1} alt="First slide" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={placeholder2}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={placeholder3} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 
